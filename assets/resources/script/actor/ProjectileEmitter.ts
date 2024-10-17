7953963c134040ace7af261ba049baaa5793d886
linkunkun <linkunkun@topjoy.com>
2024-10-17 08:42:56 +0800

    game play design

---
+++ assets/resources/script/actor/ProjectileEmitter.ts
@@ -0,0 +1,15 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('ProjectileEmmitter')
+export class ProjectileEmitter extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
+
---
+++ assets/resources/script/events/Events.ts
@@ -0,0 +1,10 @@
+import { Event } from "cc";
+
+export enum  Events {
+    OnDie = "OnDie",
+}
+
+export class CustomEventData extends Event{
+    // static TYPE : 
+}
+
